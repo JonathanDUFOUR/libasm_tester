12be0965f48197a664a372fcf9d669373fce7621
jodufour <jodufour@student.42.fr>
2024-08-29 22:35:39 +0200

    test: reworked the implementation of some existing tests
    Made changes to the helpers and the unit tests of the functions:
      - ft_strlen
      - ft_strcmp
      - ft_strcpy
      - ft_strdup
      - ft_memcpy
    to improve the consistency accross the different tests,
    and to make them easier to configure

--- tests/ft_memcpy.rs
+++ tests/ft_memcpy.rs
@@ -4,141 +4,156 @@ mod ft_memcpy {
 
 	#[link(name = "asm")]
 	extern "C" {
-		fn ft_memcpy(dst: *mut c_void, src: *const c_void, n: usize) -> *mut c_void;
+		fn ft_memcpy_dsta_srcu(dst: *mut c_void, src: *const c_void, n: usize) -> *mut c_void;
+		fn ft_memcpy_dstu_srca(dst: *mut c_void, src: *const c_void, n: usize) -> *mut c_void;
+		fn ft_memcpy_dstu_srcu(dst: *mut c_void, src: *const c_void, n: usize) -> *mut c_void;
+		// fn ft_memcpy(dst: *mut c_void, src: *const c_void, n: usize) -> *mut c_void;
+	}
+
+	const BUFFER_SIZE: usize = 1_055;
+
+	#[repr(align(32))]
+	struct AlignedBytes([u8; BUFFER_SIZE]);
+
+	impl AlignedBytes {
+		fn new() -> Self {
+			Self([0; BUFFER_SIZE])
+		}
+	}
+
+	impl std::convert::From<&[u8]> for AlignedBytes {
+		fn from(bytes: &[u8]) -> Self {
+			let mut aligned_bytes: Self = Self::new();
+
+			for (i, byte) in bytes.iter().enumerate() {
+				aligned_bytes.0[i] = *byte;
+			}
+
+			aligned_bytes
+		}
 	}
 
 	#[inline(always)]
 	fn helper(dst_len: usize, src: &[u8], n: usize) {
-		use rand::{rngs::ThreadRng, thread_rng, Rng};
+		type Function = unsafe extern "C" fn(*mut c_void, *const c_void, usize) -> *mut c_void;
 
-		const BUFFER_SIZE: usize = 1_055;
+		const FUNCTIONS: [Function; 3] =
+			[ft_memcpy_dsta_srcu, ft_memcpy_dstu_srca, ft_memcpy_dstu_srcu];
 		const ALIGN: usize = std::mem::align_of::<AlignedBytes>();
 
-		#[repr(align(32))]
-		struct AlignedBytes([u8; BUFFER_SIZE]);
-
-		impl AlignedBytes {
-			fn new() -> Self {
-				Self([0; BUFFER_SIZE])
-			}
-
-			fn from_slice(bytes: &[u8]) -> Self {
-				let mut a: Self = Self::new();
-
-				a.0[..bytes.len()].copy_from_slice(bytes);
-
-				a
-			}
-		}
-
-		fn test_with_given_offset(
-			// region: parameters
-			dst: &mut [u8],
-			src: &[u8],
-			n: usize,
-			dst_offset: usize,
-			src_offset: usize,
-			// endregion
-		) {
-			// region: body
-			let dst_p: *mut c_void = &mut dst[dst_offset] as *mut u8 as *mut c_void;
-			let src_p: *const c_void = &src[src_offset] as *const u8 as *const c_void;
-			let dst_overflow_old: Vec<u8> = dst[dst_offset + n..].to_vec();
-			let returned: *mut c_void = unsafe { ft_memcpy(dst_p, src_p, n) };
-			let returned_wrong_val: bool = returned != dst_p;
-			let dst_and_src_differ: bool =
-				dst[dst_offset..dst_offset + n] != src[src_offset..src_offset + n];
-			let wrote_out_of_range: bool = dst[dst_offset + n..] != dst_overflow_old;
-
-			if returned_wrong_val || dst_and_src_differ || wrote_out_of_range {
-				const MAX_ELEM_BY_LINE: usize = 16;
-
-				println!("dst_p: {:#X}", dst_p as usize);
-				println!("src_p: {:#X}", src_p as usize);
-				println!("    n: {}", n);
-				if returned_wrong_val {
-					println!();
-					println!("expected: {:#X}", dst_p as usize);
-					println!("returned: {:#X}", returned as usize);
-				}
-				if dst_and_src_differ {
-					println!();
-					println!("dst:");
-					for i in (0..n).step_by(MAX_ELEM_BY_LINE) {
-						println!(
-							"{:02X?}",
-							&dst[dst_offset + i..dst_offset + i + MAX_ELEM_BY_LINE.min(n - i)]
-						);
-					}
-					println!();
-					println!("src:");
-					for i in (0..n).step_by(MAX_ELEM_BY_LINE) {
-						println!(
-							"{:02X?}",
-							&src[src_offset + i..src_offset + i + MAX_ELEM_BY_LINE.min(n - i)]
-						);
-					}
-				}
-				if wrote_out_of_range {
-					println!();
-					println!("dst_overflow_old:");
-					for i in (0..BUFFER_SIZE - dst_offset - n).step_by(MAX_ELEM_BY_LINE) {
-						println!(
-							"{:02X?}",
-							&dst_overflow_old
-								[i..i + MAX_ELEM_BY_LINE.min(BUFFER_SIZE - dst_offset - n)]
-						);
-					}
-					println!();
-					println!("dst_overflow:");
-					for i in (0..BUFFER_SIZE - dst_offset - n).step_by(MAX_ELEM_BY_LINE) {
-						println!(
-							"{:02X?}",
-							&dst[dst_offset + n..]
-								[i..i + MAX_ELEM_BY_LINE.min(BUFFER_SIZE - dst_offset - n)]
-						);
-					}
-					println!();
-					println!("src_overflow:");
-					for i in (0..BUFFER_SIZE - dst_offset - n).step_by(MAX_ELEM_BY_LINE) {
-						println!(
-							"{:02X?}",
-							&src[src_offset + n..]
-								[i..i + MAX_ELEM_BY_LINE.min(BUFFER_SIZE - dst_offset - n)]
-						);
-					}
-				}
-				panic!();
-			}
-			// endregion
-		}
-
-		assert!(ALIGN <= BUFFER_SIZE, "ALIGN must be less than or equal to BUFFER_SIZE");
+		assert!(BUFFER_SIZE >= ALIGN, "BUFFER_SIZE must be greater than or equal to ALIGN");
 		assert!(
 			n <= BUFFER_SIZE - ALIGN + 1,
 			"n must be less than or equal to (BUFFER_SIZE - ALIGN + 1)"
 		);
 		assert!(n <= dst_len, "n must be less than or equal to dst_len");
+		assert!(n <= src.len(), "n must be less than or equal to src.len()");
 
-		let src_len: usize = src.len();
-		assert!(n <= src_len, "n must be less than or equal to src.len()");
+		for function in FUNCTIONS {
+			use rand::{rngs::ThreadRng, thread_rng, Rng};
 
-		let mut rng: ThreadRng = thread_rng();
-		let mut dst: AlignedBytes = AlignedBytes::new();
-		let mut src: AlignedBytes = AlignedBytes::from_slice(src);
-		let dst: &mut [u8] = &mut dst.0;
-		let src: &mut [u8] = &mut src.0;
+			#[inline(always)]
+			fn test_with_given_offset(
+				// region: parameters
+				function: Function,
+				dst: &mut [u8],
+				src: &[u8],
+				n: usize,
+				// endregion
+			) {
+				// region: body
+				let dst_p: *mut c_void = dst.as_mut_ptr() as *mut c_void;
+				let src_p: *const c_void = src.as_ptr() as *const c_void;
+				let dst_overflow: &[u8] = &dst[n..];
+				let src_overflow: &[u8] = &src[n..];
+				let dst_overflow_old: Vec<u8> = dst_overflow.to_vec();
+				let returned: *mut c_void = unsafe { function(dst_p, src_p, n) };
+				let returned_wrong_val: bool = returned != dst_p;
+				let dst_and_src_differ: bool = dst[..n] != src[..n];
+				let wrote_out_of_range: bool = dst_overflow != dst_overflow_old;
 
-		rng.fill(src[n..].as_mut());
-		rng.fill(dst[n..].as_mut());
-		for dst_offset in 0..ALIGN {
-			test_with_given_offset(dst, src, n, dst_offset, 0);
-		}
-		for src_offset in 1..ALIGN {
-			src[src_offset - 1..].copy_within(..n, 1);
+				if returned_wrong_val || dst_and_src_differ || wrote_out_of_range {
+					const MAX_ELEM_BY_LINE: usize = 16;
+
+					println!("dst_p: {:#X}", dst_p as usize);
+					println!("src_p: {:#X}", src_p as usize);
+					println!("    n: {}", n);
+					if returned_wrong_val {
+						println!();
+						println!("expected: {:#X}", dst_p as usize);
+						println!("returned: {:#X}", returned as usize);
+					}
+					if dst_and_src_differ {
+						println!();
+						println!("dst:");
+						for i in (0..n).step_by(MAX_ELEM_BY_LINE) {
+							println!("{:02X?}", &dst[i..i + MAX_ELEM_BY_LINE.min(n - i)]);
+						}
+						println!();
+						println!("src:");
+						for i in (0..n).step_by(MAX_ELEM_BY_LINE) {
+							println!("{:02X?}", &src[i..i + MAX_ELEM_BY_LINE.min(n - i)]);
+						}
+					}
+					if wrote_out_of_range {
+						let dst_overflow_old_len: usize = dst_overflow_old.len();
+						let dst_overflow_len: usize = dst_overflow.len();
+						let src_overflow_len: usize = src_overflow.len();
+
+						println!();
+						println!("dst_overflow_old:");
+						for i in (0..dst_overflow_old_len).step_by(MAX_ELEM_BY_LINE) {
+							println!(
+								"{:02X?}",
+								&dst_overflow_old
+									[i..i + MAX_ELEM_BY_LINE.min(dst_overflow_old_len - i)]
+							);
+						}
+						println!();
+						println!("dst_overflow:");
+						for i in (0..dst_overflow_len).step_by(MAX_ELEM_BY_LINE) {
+							println!(
+								"{:02X?}",
+								&dst_overflow[i..i + MAX_ELEM_BY_LINE.min(dst_overflow_len - i)]
+							);
+						}
+						println!();
+						println!("src_overflow:");
+						for i in (0..src_overflow_len).step_by(MAX_ELEM_BY_LINE) {
+							println!(
+								"{:02X?}",
+								&src_overflow[i..i + MAX_ELEM_BY_LINE.min(src_overflow_len - i)]
+							);
+						}
+					}
+					panic!();
+				}
+				// endregion
+			}
+
+			let mut rng: ThreadRng = thread_rng();
+			let mut dst: AlignedBytes = AlignedBytes::new();
+			let mut src: AlignedBytes = src.into();
+			let dst: &mut [u8] = &mut dst.0;
+			let src: &mut [u8] = &mut src.0;
+
+			rng.fill(src[n..].as_mut());
 			rng.fill(dst[n..].as_mut());
 			for dst_offset in 0..ALIGN {
-				test_with_given_offset(dst, src, n, dst_offset, src_offset);
+				let dst: &mut [u8] = &mut dst[dst_offset..];
+
+				test_with_given_offset(function, dst, src, n);
+			}
+			for src_offset in 1..ALIGN {
+				src[src_offset - 1..].copy_within(..n, 1);
+				rng.fill(dst[n..].as_mut());
+
+				let src: &[u8] = &src[src_offset..];
+
+				for dst_offset in 0..ALIGN {
+					let dst: &mut [u8] = &mut dst[dst_offset..];
+					test_with_given_offset(function, dst, src, n);
+				}
 			}
 		}
 	}
--- tests/ft_strlen.rs
+++ tests/ft_strlen.rs
@@ -4,80 +4,90 @@ mod ft_strlen {
 
 	#[link(name = "asm")]
 	extern "C" {
-		fn ft_strlen(s: *const c_char) -> usize;
+		fn ft_strlen_sa(s: *const c_char) -> usize;
+		fn ft_strlen_su(s: *const c_char) -> usize;
+		// fn ft_strlen(s: *const c_char) -> usize;
+	}
+
+	const BUFFER_SIZE: usize = 1_056;
+
+	#[repr(align(32))]
+	struct AlignedBytes([u8; BUFFER_SIZE]);
+
+	impl AlignedBytes {
+		fn new() -> Self {
+			Self([0; BUFFER_SIZE])
+		}
+	}
+
+	impl std::convert::From<&[u8]> for AlignedBytes {
+		fn from(bytes: &[u8]) -> Self {
+			let mut aligned_bytes: Self = Self::new();
+
+			for (i, byte) in bytes.iter().enumerate() {
+				aligned_bytes.0[i] = *byte;
+			}
+
+			aligned_bytes
+		}
 	}
 
 	#[inline(always)]
 	fn helper(s: &[u8]) {
-		const BUFFER_SIZE: usize = 5_120;
-		const ALIGN: usize = std::mem::align_of::<AlignedCChars>();
+		type Function = unsafe extern "C" fn(*const c_char) -> usize;
 
-		#[repr(align(4_096))]
-		struct AlignedCChars([c_char; BUFFER_SIZE]);
+		const FUNCTIONS: [Function; 2] = [ft_strlen_sa, ft_strlen_su];
+		const ALIGN: usize = std::mem::align_of::<AlignedBytes>();
 
-		impl AlignedCChars {
-			fn new() -> Self {
-				Self([0; BUFFER_SIZE])
-			}
-
-			fn from_slice(bytes: &[u8]) -> Self {
-				let mut a: Self = Self::new();
-
-				for (i, c) in bytes.iter().enumerate() {
-					a.0[i] = *c as c_char;
-				}
-
-				a
-			}
-		}
-
-		fn test_with_given_offset(
-			// region: parameters
-			s: &[c_char],
-			offset: usize,
-			len: usize,
-			// endregion
-		) {
-			// region: body
-			let p: *const c_char = &s[offset];
-			let returned: usize = unsafe { ft_strlen(p) };
-			let returned_wrong_val: bool = returned != len;
-
-			if returned_wrong_val {
-				const MAX_ELEM_BY_LINE: usize = 16;
-
-				println!("       p: {:#X}", p as usize);
-				println!();
-				println!("expected: {}", len);
-				println!("returned: {}", returned);
-				println!();
-				println!("s:");
-
-				let len: usize = len + 1;
-
-				for i in (0..len).step_by(MAX_ELEM_BY_LINE) {
-					println!("{:02X?}", &s[offset + i..offset + i + MAX_ELEM_BY_LINE.min(len - i)]);
-				}
-				panic!();
-			}
-			// endregion
-		}
-
-		assert!(ALIGN <= BUFFER_SIZE, "ALIGN must be less than or equal to BUFFER_SIZE");
-
-		let len: usize = s.len();
+		assert!(BUFFER_SIZE >= ALIGN, "BUFFER_SIZE must be greater than or equal to ALIGN");
 		assert!(
-			len <= BUFFER_SIZE - ALIGN,
+			s.len() <= BUFFER_SIZE - ALIGN,
 			"s.len() must be less than or equal to (BUFFER_SIZE - ALIGN)"
 		);
 
-		let mut s: AlignedCChars = AlignedCChars::from_slice(s);
-		let s: &mut [c_char] = &mut s.0;
+		let len: usize = s.len();
 
-		test_with_given_offset(s, 0, len);
-		for offset in 1..ALIGN {
-			s[offset - 1..].copy_within(..len, 1);
-			test_with_given_offset(s, offset, len);
+		for function in FUNCTIONS {
+			fn test_with_given_offset(
+				// region: parameters
+				function: Function,
+				s: &[u8],
+				len: usize,
+				// endregion
+			) {
+				// region: body
+				let p: *const c_char = s.as_ptr() as *const c_char;
+				let returned: usize = unsafe { function(p) };
+				let returned_wrong_val: bool = returned != len;
+
+				if returned_wrong_val {
+					const MAX_ELEM_BY_LINE: usize = 16;
+
+					println!("       p: {:#X}", p as usize);
+					println!();
+					println!("expected: {}", len);
+					println!("returned: {}", returned);
+					println!();
+					println!("s:");
+
+					let len: usize = len + 1;
+
+					for i in (0..len).step_by(MAX_ELEM_BY_LINE) {
+						println!("{:02X?}", &s[i..i + MAX_ELEM_BY_LINE.min(len - i)]);
+					}
+					panic!();
+				}
+				// endregion
+			}
+
+			let mut s: AlignedBytes = s.into();
+			let s: &mut [u8] = &mut s.0;
+
+			test_with_given_offset(function, s, len);
+			for offset in 1..ALIGN {
+				s[offset - 1..].copy_within(..len, 1);
+				test_with_given_offset(function, s[offset..].as_ref(), len);
+			}
 		}
 	}
 
