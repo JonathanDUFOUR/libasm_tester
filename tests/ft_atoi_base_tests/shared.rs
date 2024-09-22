64340633f941b28f65f4e4bfd34f322760c130e6
jodufour <jodufour@student.42.fr>
2024-09-22 19:56:58 +0200

    refactor: rename symbols in the ft_atoi_base helper
    Renamed each symbol related to the base from `base[_suffix]`
    to `b[_suffix]`. This makes them as long as the symbols
    related to the string to parse, and so it makes the code
    more pleasant to read.

--- tests/ft_atoi_base_tests/shared.rs
+++ tests/ft_atoi_base_tests/shared.rs
@@ -2,7 +2,7 @@ use std::ffi::{c_char, c_int};
 
 #[link(name = "asm_bonus")]
 extern "C" {
-	fn ft_atoi_base(s: *const c_char, base: *const c_char) -> c_int;
+	fn ft_atoi_base(s: *const c_char, b: *const c_char) -> c_int;
 }
 
 const BUFFER_SIZE: usize = 1_057;
@@ -28,7 +28,7 @@ impl std::convert::From<&[u8]> for AlignedBytes {
 	}
 }
 
-pub fn helper(s: &[u8], base: &[u8], expected: c_int) {
+pub fn helper(s: &[u8], b: &[u8], expected: c_int) {
 	type Function = unsafe extern "C" fn(*const c_char, *const c_char) -> c_int;
 
 	const FUNCTIONS: [Function; 1] = [ft_atoi_base];
@@ -40,12 +40,12 @@ pub fn helper(s: &[u8], base: &[u8], expected: c_int) {
 		"s.len() must be lower than or equal to (BUFFER_SIZE - ALIGN)"
 	);
 	assert!(
-		base.len() <= BUFFER_SIZE - ALIGN,
-		"base.len() must be lower than or equal to (BUFFER_SIZE - ALIGN)"
+		b.len() <= BUFFER_SIZE - ALIGN,
+		"b.len() must be lower than or equal to (BUFFER_SIZE - ALIGN)"
 	);
 
 	let s_len: usize = s.len() + 1;
-	let base_len: usize = base.len() + 1;
+	let b_len: usize = b.len() + 1;
 
 	for function in FUNCTIONS {
 		use rand::{rngs::ThreadRng, thread_rng, Rng};
@@ -55,22 +55,22 @@ pub fn helper(s: &[u8], base: &[u8], expected: c_int) {
 			// region: parameters
 			function: Function,
 			s: &[u8],
-			base: &[u8],
+			b: &[u8],
 			s_len: usize,
-			base_len: usize,
+			b_len: usize,
 			expected: c_int,
 			// endregion
 		) {
 			// region: body
 			let s_ptr: *const c_char = s.as_ptr() as *const c_char;
-			let base_ptr: *const c_char = base.as_ptr() as *const c_char;
-			let returned: c_int = unsafe { function(s_ptr, base_ptr) };
+			let b_ptr: *const c_char = b.as_ptr() as *const c_char;
+			let returned: c_int = unsafe { function(s_ptr, b_ptr) };
 
 			if returned != expected {
 				const MAX_ELEM_BY_LINE: usize = 16;
 
-				println!("   s_ptr: {:#X}", s_ptr as usize);
-				println!("base_ptr: {:#X}", base_ptr as usize);
+				println!("s_ptr: {:#X}", s_ptr as usize);
+				println!("b_ptr: {:#X}", b_ptr as usize);
 				println!();
 				println!("expected: {}", expected);
 				println!("returned: {}", returned);
@@ -80,9 +80,9 @@ pub fn helper(s: &[u8], base: &[u8], expected: c_int) {
 					println!("{:02X?}", &s[i..i + MAX_ELEM_BY_LINE.min(s_len - i)]);
 				}
 				println!();
-				println!("base:");
-				for i in (0..base_len).step_by(MAX_ELEM_BY_LINE) {
-					println!("{:02X?}", &base[i..i + MAX_ELEM_BY_LINE.min(base_len - i)]);
+				println!("b:");
+				for i in (0..b_len).step_by(MAX_ELEM_BY_LINE) {
+					println!("{:02X?}", &b[i..i + MAX_ELEM_BY_LINE.min(b_len - i)]);
 				}
 				panic!();
 			}
@@ -91,33 +91,33 @@ pub fn helper(s: &[u8], base: &[u8], expected: c_int) {
 
 		let mut rng: ThreadRng = thread_rng();
 		let mut s: AlignedBytes = s.into();
-		let mut base: AlignedBytes = base.into();
+		let mut b: AlignedBytes = b.into();
 		let s: &mut [u8] = &mut s.0;
-		let base: &mut [u8] = &mut base.0;
+		let b: &mut [u8] = &mut b.0;
 
 		rng.fill(s[s_len..].as_mut());
-		rng.fill(base[base_len..].as_mut());
-		test_with_given_offsets(function, s, base, s_len, base_len, expected);
+		rng.fill(b[b_len..].as_mut());
+		test_with_given_offsets(function, s, b, s_len, b_len, expected);
 		for s_offset in 1..ALIGN {
 			s[s_offset - 1..].copy_within(..s_len, 1);
 
 			let s: &[u8] = &s[s_offset..];
 
-			test_with_given_offsets(function, s, base, s_len, base_len, expected);
+			test_with_given_offsets(function, s, b, s_len, b_len, expected);
 		}
-		for base_offset in 1..ALIGN {
+		for b_offset in 1..ALIGN {
 			s.copy_within(ALIGN - 1..ALIGN - 1 + s_len, 0);
-			base[base_offset - 1..].copy_within(..base_len, 1);
+			b[b_offset - 1..].copy_within(..b_len, 1);
 
-			let base: &[u8] = &base[base_offset..];
+			let b: &[u8] = &b[b_offset..];
 
-			test_with_given_offsets(function, s, base, s_len, base_len, expected);
+			test_with_given_offsets(function, s, b, s_len, b_len, expected);
 			for s_offset in 1..ALIGN {
 				s[s_offset - 1..].copy_within(..s_len, 1);
 
 				let s: &[u8] = &s[s_offset..];
 
-				test_with_given_offsets(function, s, base, s_len, base_len, expected);
+				test_with_given_offsets(function, s, b, s_len, b_len, expected);
 			}
 		}
 	}
